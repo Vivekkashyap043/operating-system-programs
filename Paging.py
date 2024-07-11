917a3d7e037dff76ac9d265da8b112526a034ff7
vivek <vivekkashyap043@gmail.com>
2024-07-11 12:41:44 +0530

    paging added

--- Paging.py
+++ Paging.py
@@ -1,28 +1,23 @@
-msize = int(input("Enter the memory size: "))
-psize = int(input("Enter the page size: "))
-nopages = msize // psize
-print("The number of pages available in memory are:", nopages)
-np = int(input("Enter the number of processes: "))
-pages = [0] * (nopages + 1)
-process = [0] * (np + 1)
-k = 1
-j = 0 # Define j here
-for i in range(np):
-    x = int(input(f"Enter number of pages required for P[{i + 1}]: "))
-    process[k] = j + 1
-    k += 1
- 
-    if j + x <= nopages:
-        print(f"Enter page table for P[{i + 1}]:")
-        for _ in range(x):
-            p = int(input())
-            j += 1
-            pages[j] = p
-    else:
-        print("Memory is Full")
-        break
+import random
 
-print("Logical Address to find Physical Address")
-pno, pageno, offset = map(int, input("Enter process number, page number, and offset: ").split())
-paddress = pages[process[pno] + pageno - 1] * psize + offset
-print("The physical address is:", paddress)
+n=int(input('Enter Memory Size:'))
+fr_size=int(input('Enter Frame Size:'))
+no_of_frames=n//fr_size
+frames=[random.randint(0,1) for _ in range(no_of_frames)]
+no_process = int(input("Enter no. of processes: "))
+process_pages = []
+for i in range(no_process):
+    no_pages = int(input(f"Enter no. of pages of process {i+1}: "))
+    process_pages.append(no_pages)
+
+for p in range(no_process):
+    print("Process - "+str(p+1))
+    for page in range(process_pages[p]):
+        free_all = [i for i,frame in enumerate(frames) if frame == 0]
+        if len(free_all) > 0:
+            idx = free_all.pop(0)
+            frames[idx] = 1
+            print("Page-"+str(page+1)+" in frame "+str(idx))
+        else:
+            print("Page-"+str(page+1)+" not allocated")
+        
